b6de88bb5fb4c47826f7b94b080b10d0dc493b0e
mansurrv <buyserikov06@gmail.com>
2025-12-12 22:02:47 +0500

    Update

---
+++ index.js
@@ -0,0 +1,22 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer((req, res) => {
+    let url = req.url;
+    if (url.split("/")[1] === "public") {
+        handlePublic(url, req, res);
+    }
+    else {
+        let html = fs.readFileSync("./index.html");
+        res.setHeader("Content-type", "text/html");
+        res.statusCode = 200;
+        res.end(html);
+    }
+})
+
+function handlePublic(url, req, res) {
+    let file = fs.readFileSync("./" + url);
+    res.end(file);
+}
+
+server.listen(3000);
